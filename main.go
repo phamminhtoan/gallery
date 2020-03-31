457a48d04237bb825aa1eaec806008cdbb9d9cab
Pham Minh Toan <toanphamminh@outlook.com.vn>
2020-03-31 22:55:02 +0700

    update

--- main.go
+++ main.go
@@ -2,21 +2,67 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello there, Wellcome to my page!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an mail to <a href=\"mailto:minhtoan.jc@gmail.com\">minhtoan.jc@gmail.com</a>.")
-	} else {
-		fmt.Fprint(w, r.URL.Path)
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
 	}
+	// fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+	fmt.Fprint(w, "To get in touch, please send an mail to <a href=\"mailto:minhtoan.jc@gmail.com\">minhtoan.jc@gmail.com</a>.")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>This is FAQ page!</h1>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1 style=\"color:red\">Sorry, We couldn't find the page you were looking for</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	var err error
+	homeTemplate, err = template.ParseFiles(
+		"views/home.html",
+		"views/layouts/footer.html",
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles(
+		"views/contact.html",
+		"views/layouts/footer.html",
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	http.ListenAndServe(":3000", r)
 }
